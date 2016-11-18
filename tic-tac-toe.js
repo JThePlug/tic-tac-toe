8b3271ce5d360fa54961f105ebe32f7e42c4a164
Joshua Banuelos <DoctorDamage0524@gmail.com>
2016-11-18 10:16:19 -0800

    iugjgfyigigiy

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,81 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var turn = "player1"
+
+funtion shape(){
+  is(turn=="player1"){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+circle.setAttribute.getElementById("game-board")
+canvas.appenedChild(circle)
+turn = "player2"}else{car circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+var canvas = document.getElementById("game-board")
+canvas.appenedChild(circle)
+turn-="player1"
+}
+}
+funtion shape(){
+  is(turn=="player1"){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+circle.setAttribute.getElementById("game-board")
+canvas.appenedChild(circle)
+turn = "player2"}else{car circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+var canvas = document.getElementById("game-board")
+canvas.appenedChild(circle)
+turn-="player1"
+}
+}
+funtion shape(){
+  is(turn=="player1"){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+circle.setAttribute.getElementById("game-board")
+canvas.appenedChild(circle)
+turn = "player2"}else{car circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+var canvas = document.getElementById("game-board")
+canvas.appenedChild(circle)
+turn-="player1"
+}
+}
+funtion shape(){
+  is(turn=="player1"){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+  circle.setAttribute("cy,")
+circle.setAttribute.getElementById("game-board")
+canvas.appenedChild(circle)
+turn = "player2"}else{car circle = document.createElementNS(namespace, "circle")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+circle.setAttribute("cy,")
+var canvas = document.getElementById("game-board")
+canvas.appenedChild(circle)
+turn-="player1"
+}
+}
